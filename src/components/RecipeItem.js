ca96a24f46b65cd4e999331a10f17348cb365c3e
Marie Røgler <mroegler@gmail.com>
2020-10-10 10:15:17 +0200

    ÂÂÂcommit recipeitem.js

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,22 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { BASE_URL } from "./api";
 
 function RecipeItem({ title, thumbnail   }) {
-    /*return (
-        <Recipe>
-            <Recipe.Img variant="top" src={image} />
-            <Recipe.Body>
-                <Recipe.Title>{name}</Recipe.Title>
-                <Link to={"Recipe/" + id}>
-                    <Button variant="secondary" block>
-                        View
-                    </Button>
-                </Link>
-            </Recipe.Body>
-        </Recipe>
-    );*/
-    return <p>Hello</p>
+   
+    return (
+        <div>
+            <p>{title}</p>
+            <img src={thumbnail} alt={title} />
+        </div>
+    )
+    
 }
 
 RecipeItem.propTypes = {
